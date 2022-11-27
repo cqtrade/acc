ad147df0e1f3a9a6edbcabdf21e74aa5adb6d40e
Sander Mets <sander.mets@builderhead.io>
2022-11-27 11:03:24 +0200

    Define data of candles

--- src/api.js
+++ src/api.js
@@ -10,13 +10,36 @@ async function getAccountInfo() {
     console.log(res)
 }
 
-async function getCandles() {
+/**
+ * 
+ * Kline/Candlestick Data<br>
+ *
+ * GET /api/v3/klines<br>
+ *
+ * {@link https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data}
+ */
+async function getCandles(symbol, interval, opts) {
     try {
-        const res = await client.klines('BTCUSDT', '1h')
-        console.log(res)
+        const { data } = await client.klines(symbol, interval, opts)
+        return data.map(d => ({
+            startTime: d[0],
+            startTimeString: new Date(d[0]),
+            open: Number(d[1]),
+            high: Number(d[2]),
+            low: Number(d[3]),
+            close: Number(d[4]),
+            volume: Number(d[5]),
+            endTime: d[6],
+            endTimeString: new Date(d[6]),
+        }))
     } catch (error) {
-        console.log('ERROR', error)
+        console.log('ERROR getCandles', error)
+        throw error
     }
 }
 
-// getCandles().then()
+exports.getCandles = getCandles
+
+// getCandles('BTCUSDT', '1h', { limit: 5 })
+//     .then(candles => console.log(candles))
+//     .catch()
